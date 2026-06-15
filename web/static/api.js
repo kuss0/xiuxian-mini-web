b34f7ab7b82a7f3682e3657cbf15610e6e187f11
root <root@pve.localdomain>
2026-06-16 01:11:47 +0800

    Remove frontend token prompt

--- web/static/api.js
+++ web/static/api.js
@@ -2,8 +2,6 @@
 (function () {
   "use strict";
 
-  const AUTH_TOKEN_STORAGE_KEY = "xiuxianMiniwebAccessToken";
-
   async function fetchJson(url) {
     const response = await apiFetch(url);
     if (!response.ok) {
@@ -24,31 +22,8 @@
     return response.json();
   }
 
-  async function apiFetch(url, options = {}, allowRetry = true) {
-    const response = await fetch(url, {
-      ...options,
-      headers: authHeaders(options.headers),
-    });
-    if (response.status !== 401 || !allowRetry) {
-      return response;
-    }
-
-    sessionStorage.removeItem(AUTH_TOKEN_STORAGE_KEY);
-    const token = window.prompt("请输入 Mini Web 访问口令");
-    if (!token) {
-      return response;
-    }
-    sessionStorage.setItem(AUTH_TOKEN_STORAGE_KEY, token.trim());
-    return apiFetch(url, options, false);
-  }
-
-  function authHeaders(headers = {}) {
-    const merged = { ...headers };
-    const token = sessionStorage.getItem(AUTH_TOKEN_STORAGE_KEY);
-    if (token) {
-      merged["X-Miniweb-Token"] = token;
-    }
-    return merged;
+  async function apiFetch(url, options = {}) {
+    return fetch(url, options);
   }
 
   window.MiniwebApi = {
